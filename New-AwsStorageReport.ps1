5a2a7f9c05b8e404c92932863c8732b1487e2692
Kollen Hensley <kollenh@slalom.com>
2023-03-28 14:09:09 -0700

    Move snapshot output to object

--- New-AwsStorageReport.ps1
+++ New-AwsStorageReport.ps1
@@ -38,43 +38,58 @@
 
     $Volume_List   = [System.Collections.ArrayList]::New()
     $Snapshot_List = [System.Collections.ArrayList]::New()
-    foreach ($Region in $RegionList) {
-        Initialize-AWSDefaultconfiguration -ProfileName MyDefault -Region $Region
-
-
-        # Get the data
-#        $Tags       = Get-EC2Tag
-        $Volumes    = Get-EC2Volume
-        $Snapshots  = Get-EC2Snapshots -OwnerId self -Region $Region
-    
-        foreach ($V in $Volumes) {
-            $SnapCount = ($Snapshots | Where-Object VolumeId -eq $($v.VolumeId) | Measure-Object).Count
-            $Object = [PSCustomObject]@{
-                Region      = $Region
-                VolID       = $V.VolumeId
-                Name        = ($V.Tags | Where-Object Key -eq 'Name').Value
-                CreateTime  = $V.CreateTime
-                Encrypted   = $V.Encrypted
-                State       = $V.State
-                VolumeType  = $V.VolumeType
-                Size        = $V.Size
-                Snapshots   = $SnapCount
-            }
-            $Volume_List.Add($Object) | Out-Null
-            $SnapShotTotal  = $SnapShotTotal + $SnapCount
+    foreach ($account in $AccountID) {
+        Switch ($Account) {
+            '252302356329'  {$AWS_Profile = 'SecOps'}
+            '427878221502'  {$AWS_Profile = 'InfraProd'}
+            default         {$AWS_Profile = 'MyDefault'}
         }
+        
+        foreach ($Region in $RegionList) {
+            Initialize-AWSDefaultconfiguration -ProfileName $AWS_Profile -Region $Region
 
-        $Grouped_Snapshots  = $Snapshots | Group-Object VolumeId | Select-Object `
-            Count,
-            Name,
-            @{Label='Description';Expression={$_.Group[-1].Description}},
-            @{Label='Size';Expression={$_.Group[-1].VolumeSize}},
-            @{Label='Tier';Expression={$_.Group[-1].StorageTier}},
-            @{Label='State';Expression={$_.Group[-1].State}},
-            @{Label='Started';Expression={$_.Group[-1].StartTime}}
-        $Snapshot_List.Add($Grouped_Snapshots) | Out-Null
+            # Get the data
+    #        $Tags       = Get-EC2Tag
+            $Volumes    = Get-EC2Volume
+            $Snapshots  = Get-EC2Snapshots -OwnerId self -Region $Region
+        
+            foreach ($V in $Volumes) {
+                $SnapCount = ($Snapshots | Where-Object VolumeId -eq $($v.VolumeId) | Measure-Object).Count
+                $Object = [PSCustomObject]@{
+                    Account     = $Account
+                    Region      = $Region
+                    VolID       = $V.VolumeId
+                    Name        = ($V.Tags | Where-Object Key -eq 'Name').Value
+                    CreateTime  = $V.CreateTime
+                    Encrypted   = $V.Encrypted
+                    State       = $V.State
+                    VolumeType  = $V.VolumeType
+                    Size        = $V.Size
+                    Snapshots   = $SnapCount
+                }
+                $Volume_List.Add($Object) | Out-Nul
+                $SnapShotTotal  = $SnapShotTotal + $SnapCount
+            }
+
+            $Snapshots | Group-Object VolumeId | Foreach-Object {
+                $Grouped_Snapshot = [PSCustomObject]@{
+                    Account     = $AccountID
+                    Region      = $Region
+                    Number      = $_.Count
+                    Name        = $_.Name
+                    Description = $_.Group[-1].Description
+                    Size        = $_.Group[-1].VolumeSize
+                    Tier        = $_.Group[-1].StorageTier
+                    State       = $_.Group[-1].State
+                    Started     = $_.Group[-1].StartTime
+                }
+                $Snapshot_List.Add($Grouped_Snapshot) | Out-Null
+            }
+        }
     }
+    
     Write-Host "Volumes accounted for $('{0:N0}' -f $SnapShotTotal) snapshots" -ForegroundColor Cyan
+
     $Volume_List
     $Snapshot_List
     
